93771870a3c23bd29a7fbc6f16e6433a3b4cff47
aprizal24 <127104639+aprizal24@users.noreply.github.com>
2023-10-26 10:12:20 +0700

    Modul3ProgramWeb

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from '/Users/rian/Documents/MyDoc/Semester 5/project/src/components/Navbar.jsx';
+import Footer from '/Users/rian/Documents/MyDoc/Semester 5/project/src/components/Footer.jsx';
+import Home from '/Users/rian/Documents/MyDoc/Semester 5/project/src/pages/Home.jsx';
+import AboutUs from '/Users/rian/Documents/MyDoc/Semester 5/project/src/pages/AboutUs.jsx';
+import Contact from '/Users/rian/Documents/MyDoc/Semester 5/project/src/pages/Contact.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
+
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from '/Users/rian/Documents/MyDoc/Semester 5/project/src/components/Hero.jsx';
+
+function Home() {
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
+}
+
+export default Home;
